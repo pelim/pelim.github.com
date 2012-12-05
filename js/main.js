342e516bbd6569b81117749bb4960a8055ead4d3
pelim <peter.limbach@gmail.com>
2012-12-05 16:54:01 +0100

    add git homepage

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$('.cursor').blink();
+	
+	$('#imprint').click( function(e){
+		e.preventDefault();
+		$('section.content-wrapper').addClass('hidden');
+		$('section.content-wrapper.imprint').removeClass('hidden');
+	})
+	
+	$('#home').click( function(e){
+		e.preventDefault();
+		$('section.content-wrapper').addClass('hidden');
+		$('section.content-wrapper.home').removeClass('hidden');
+	})
+});
+
+(function($){
+	$.fn.blink = function(options) {
+		var element = this;
+	    var settings = $.extend({
+			interval: 100,
+			fade: 700
+		}, options);
+		
+		timer = setInterval(_blink, settings.interval);
+		function _blink() {
+			element.fadeOut(settings.fade, function() {
+				element.fadeIn(settings.fade);
+			});
+	    }
+	};
+})(jQuery);
