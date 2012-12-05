e37bfa4c4148617a95f05fa45433abe02a9e4ddd
pelim <peter.limbach@gmail.com>
2012-12-05 17:53:27 +0100

    add german version

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
 $(document).ready(function() {
+
 	$('.cursor').blink();
 	
-	$('#imprint').click( function(e){
+	$('#imprint').click(function(e){
 		e.preventDefault();
 		$('section.content-wrapper').addClass('hidden');
 		$('section.content-wrapper.imprint').removeClass('hidden');
+	});
+	
+	$('#german').click(function(e) {
+		e.preventDefault();
+		$(this).addClass('hidden');
+		$('#english').removeClass('hidden');
+		$('section.content-wrapper').addClass('hidden');
+		$('section.content-wrapper.german.home').removeClass('hidden');
+	});
+	
+	$('#english').click(function(e) {
+		e.preventDefault();
+		$(this).addClass('hidden');
+		$('#german').removeClass('hidden');
+		$('section.content-wrapper').addClass('hidden');
+		$('section.content-wrapper.english.home').removeClass('hidden');
 	})
 	
-	$('#home').click( function(e){
+	$('#home').click(function(e){
 		e.preventDefault();
 		$('section.content-wrapper').addClass('hidden');
-		$('section.content-wrapper.home').removeClass('hidden');
-	})
+		$('section.content-wrapper.english.home').removeClass('hidden');
+	});
+	
+	
 });
 
 (function($){
